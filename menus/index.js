c9c98954c7c6c01825e0461074188acfafecf72b
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-10 21:29:25 -0700

    Created indexMenu with generic switch

---
+++ menus/index.js
@@ -0,0 +1,46 @@
+//this will be the index menu
+//inquirer
+const inquirer = require("inquirer");
+
+const questions = [
+    {
+        type: "list",
+        name: "indexChoice",
+        message: "What would you like to do?",
+        choices: ["View All Departments", "View All Roles", "View All Employees", "Add A Department", "Add A Role", "Add An Employee", "Update An Employee Role"]
+    }
+];
+
+function indexMenu () {
+    inquirer.prompt(questions).then((data) => {
+        switch (data.indexChoice) {
+            case "View All Departments":
+                console.log("navigated to view all departments")
+                break;
+            case "View All Roles":
+                console.log("navigated to View All Roles")
+                break;
+            case "View All Employees":
+                console.log("navigated to View All Employees")
+                break;
+            case "Add A Department":
+                console.log("navigated to Add A Department")
+                break;
+            case "Add A Role":
+                console.log("navigated to Add A Role")
+                break;
+            case "Add An Employee":
+                console.log("navigated to Add An Employee")
+                break;
+            case "Update An Employee Role":
+                console.log("navigated to Update An Employee Role")
+                break;
+        
+            default:
+                console.log("error")
+                break;
+        }
+    })
+}
+
+module.exports = indexMenu
