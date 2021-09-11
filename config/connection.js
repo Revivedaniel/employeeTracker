d335a34ed0c247744bb5ea91b224d589a639416c
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-10 20:49:49 -0700

    Created connection.js using process.env names

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: process.env.DB_USER,
+      // TODO: Add MySQL password here
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    console.log(`Connected to the ${process.env.DB_NAME} database.`)
+  );
+
+module.exports = db;
