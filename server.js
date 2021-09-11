37eddeab611a31785a2897b8edd70a365aa66e93
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-10 21:30:13 -0700

    un required inquirer, required indexMenu and called it in init

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-
-//inquirer
-const inquirer = require("inquirer");
+const { query } = require("./config/connection.js");
 //connection
 const db = require("./config/connection.js")
+//indexMenu
+const indexMenu = require("./menus/index.js")
 
 function init () {
-    
+    indexMenu()
 }
 
 init();
