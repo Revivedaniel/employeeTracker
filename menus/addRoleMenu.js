39008aa324a78e9cef56011607edc56bae65f478
Revivedaniel <mlg.danielstark@gmail.com>
2021-09-12 22:17:01 -0700

    Created inquirer menu for addARole

---
+++ menus/addRoleMenu.js
@@ -0,0 +1,54 @@
+//inquirer
+const inquirer = require("inquirer");
+//AddADepartment
+const addARole = require("../lib/addARole");
+
+const currentDepartments = [];
+const currentDepartmentIds = {};
+
+const questions = [
+  {
+    type: "input",
+    name: "roleName",
+    message: "What is the name of the Role you want to add?",
+  },
+  {
+    type: "input",
+    name: "roleSalary",
+    message: "What is the Salary of the role?",
+  },
+  {
+    type: "list",
+    name: "roleDepartment",
+    message: "What Department does the Role belong to?",
+    choices: currentDepartments,
+  },
+];
+
+function addRoleMenu() {
+  //db connection
+  const db = require("../config/connection");
+  const sql = "SELECT * FROM departments";
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    for (const key in results) {
+      const element = results[key];
+        currentDepartments.push(element.department_name)
+        currentDepartmentIds[element.department_name] = element.id
+    }
+  });
+
+  inquirer.prompt(questions).then((data) => {
+      console.log(currentDepartments)
+      console.log(currentDepartmentIds)
+    const indexMenu = require("../menus/index");
+    const role_department_id = currentDepartmentIds[data.roleDepartment]
+    addARole(data.roleName, data.roleSalary, role_department_id);
+    indexMenu();
+  });
+}
+
+module.exports = addRoleMenu;
